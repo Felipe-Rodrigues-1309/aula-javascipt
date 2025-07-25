a6f528f3aea6cd42fc17038d6ca35f7e00e6fa29
Felipe Rodrigues <feliphi10@gmail.com>
2025-07-25 00:00:13 -0300

    24072025

---
+++ SECCAO_08/01_metodos/scripts.js
@@ -0,0 +1,21 @@
+// Criação de um objeto chamado 'cachorro' exibindo no input 
+let cachorro = {
+  // Definição de um método chamado 'latir'
+  latir: function() {
+    // Cria uma variável local chamada 'auau' e atribui a ela a string "au au au"
+    let auau = "au au au";
+    // Seleciona o elemento HTML com a classe 'butao' e altera seu valor para o conteúdo da variável 'auau'
+    // Isso normalmente afeta o texto exibido em um input do tipo button ou text
+    document.querySelector(".butao").value = auau;
+  }
+}
+
+
+// outra forma de funcao com metodo exibindo no console
+let carro = {
+    buzinar: function(){
+        console.log("bi bi ");
+    }
+}
+
+carro.buzinar();
